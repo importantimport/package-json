9a2336d6c06bd150294653f05f7b1443a42cc5ee
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-29 21:48:51 +0800

    fix(sort/bin): remove args

--- packages/sort/src/bin.ts
+++ packages/sort/src/bin.ts
@@ -1,25 +1,8 @@
 import { glob, readFile, writeFile } from 'node:fs/promises'
-import { cwd } from 'node:process'
-import { parseArgs } from 'node:util'
 
 import { sort } from './lib/sort'
 
-const args = parseArgs({
-  args: ['--cwd', '--glob'],
-  options: {
-    cwd: {
-      default: cwd(),
-      type: 'string',
-    },
-    glob: {
-      default: '**/package.json',
-      type: 'string',
-    },
-  },
-})
-
-for await (const entry of glob(args.values.glob ?? '**/package.json', {
-  cwd: args.values.cwd,
+for await (const entry of glob('**/package.json', {
   exclude: fileName => !fileName.endsWith('package.json'),
 })) {
   const text = await readFile(entry, { encoding: 'utf8' })
