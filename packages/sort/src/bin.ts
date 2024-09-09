8d4a8e57a574493d0181298d6bce22f4a2850f05
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-09 20:17:38 +0800

    fix(sort): remove exclude

--- packages/sort/src/bin.ts
+++ packages/sort/src/bin.ts
@@ -3,7 +3,7 @@ import { glob, readFile, writeFile } from 'node:fs/promises'
 import { sort } from './lib/sort'
 
 for await (const entry of glob('**/package.json', {
-  exclude: fileName => !fileName.endsWith('package.json'),
+  // exclude: fileName => !fileName.endsWith('package.json'),
 })) {
   const text = await readFile(entry, { encoding: 'utf8' })
   const json = JSON.parse(text)
