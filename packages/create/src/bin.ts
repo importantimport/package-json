9f6818e5d84ec1d1ec5c73044719f8efa6568c66
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-09 19:23:52 +0800

    fix(create): add eol

--- packages/create/src/bin.ts
+++ packages/create/src/bin.ts
@@ -2,4 +2,4 @@ import { writeFile } from 'node:fs/promises'
 
 import { create } from './lib/create'
 
-await writeFile('package.json', JSON.stringify(create(), null, 2))
+await writeFile('package.json', `${JSON.stringify(create(), null, 2)}\n`)
