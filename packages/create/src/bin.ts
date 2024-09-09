a333086bae41399d2dfce39a2f03559e12c3b46c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-09 20:09:28 +0800

    feat(create): add log

--- packages/create/src/bin.ts
+++ packages/create/src/bin.ts
@@ -3,3 +3,5 @@ import { writeFile } from 'node:fs/promises'
 import { create } from './lib/create'
 
 await writeFile('package.json', `${JSON.stringify(create(), null, 2)}\n`)
+// eslint-disable-next-line no-console
+console.log('✔ Created package.json')
