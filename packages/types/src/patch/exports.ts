9c81388a9e08f3cd8a4f24eb26959216fdec817c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-16 21:28:57 +0800

    fix(types): fix ts error

--- packages/types/src/patch/exports.ts
+++ packages/types/src/patch/exports.ts
@@ -1,6 +1,14 @@
 import type { PackageExportsEntry, PackageExportsEntryObject1, PackageExportsFallback, PackageExportsFallback1 } from '../types'
 
 /**
+ * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
+ *
+ * This interface was referenced by `undefined`'s JSON-Schema definition
+ * via the `patternProperty` "^\./.+".
+ */
+type PatchedPackageExportsEntry = Record<`./${string}` | ({} & string), PackageExportsEntry | PackageExportsFallback>
+
+/**
  * The "exports" field is used to restrict external access to non-exported module files, also enables a module to import itself using "name".
  */
 export type PatchedExports =
@@ -10,14 +18,7 @@ export type PatchedExports =
      */
     '.'?: PackageExportsEntry | PackageExportsFallback
     './package.json'?: './package.json'
-    /**
-     * The module path prefix that is resolved when the module specifier starts with "name/", set to "./*" to allow external modules to import any subpath.
-     *
-     * This interface was referenced by `undefined`'s JSON-Schema definition
-     * via the `patternProperty` "^\./.+".
-     */
-    [k: `./${string}` | ({} & string)]: PackageExportsEntry | PackageExportsFallback
-  }
+  } & PatchedPackageExportsEntry
   | (null | string)
   | PackageExportsEntryObject1
   | PackageExportsFallback1
