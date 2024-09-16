20a955fd01dc0ebfa93a2cf1f3e600d8d05bd0e4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-16 23:04:06 +0800

    fix(types): simplify exports

--- packages/types/src/patch/exports.ts
+++ packages/types/src/patch/exports.ts
@@ -1,17 +1,6 @@
 import type { XOR } from '../lib/xor'
 
 /**
- A mapping of conditions and the paths to which they resolve.
- */
-interface ExportConditions {
-  [condition: string]: Exports
-}
-
-/**
  * The "exports" field is used to restrict external access to non-exported module files, also enables a module to import itself using "name".
  */
-export type Exports = null | XOR<
-  string,
-  Array<ExportConditions | string>,
-  ExportConditions
->
+export type Exports = null | XOR<Record<string, unknown>, string, string[]>
