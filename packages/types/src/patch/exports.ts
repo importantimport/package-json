0b308db1b1ebbba3a622382dee1cf71f197fbe1a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-18 20:19:08 +0800

    refactor(types): update exports type

--- packages/types/src/patch/exports.ts
+++ packages/types/src/patch/exports.ts
@@ -1,6 +1,19 @@
 import type { XOR } from '../lib/xor'
 
+interface Export {
+  [k: string]: Export | string | undefined
+  default?: Export | string
+  import?: Export | string
+  require?: Export | string
+  types?: Export | string
+}
+
+interface TopLevelExport extends Record<`./${string}`, Export | string | undefined> {
+  '.'?: Export | string
+  './package.json'?: './package.json'
+}
+
 /**
  * The "exports" field is used to restrict external access to non-exported module files, also enables a module to import itself using "name".
  */
-export type Exports = null | XOR<Record<string, unknown>, `./${string}`, `./${string}`[]>
+export type Exports = null | XOR<XOR<TopLevelExport, Export>, `./${string}`, `./${string}`[]>
