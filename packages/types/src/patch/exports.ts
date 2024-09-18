0209bb2f269a82454259fe139257217d9a685c48
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-18 20:05:00 +0800

    refactor(types): update exports type

--- packages/types/src/patch/exports.ts
+++ packages/types/src/patch/exports.ts
@@ -3,4 +3,4 @@ import type { XOR } from '../lib/xor'
 /**
  * The "exports" field is used to restrict external access to non-exported module files, also enables a module to import itself using "name".
  */
-export type Exports = null | XOR<Record<string, unknown>, string, string[]>
+export type Exports = null | XOR<Record<string, unknown>, `./${string}`, `./${string}`[]>
