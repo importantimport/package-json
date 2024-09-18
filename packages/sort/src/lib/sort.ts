02f955eda5822a0fea38f1b8356cf43d323bacef
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-18 20:54:40 +0800

    feat(sort): support string

--- packages/sort/src/lib/sort.ts
+++ packages/sort/src/lib/sort.ts
@@ -5,9 +5,13 @@ import { order, orderKeys } from './order'
  * @param packageJson package.json
  * @returns sorted package.json
  */
-export const sort = (packageJson: Record<string, unknown>) =>
+export const sort = (packageJson: Record<string, unknown> | string): Record<string, unknown> =>
   Object.fromEntries(
-    Object.entries(packageJson)
+    Object.entries(
+      packageJson instanceof Object
+        ? packageJson
+        : JSON.parse(packageJson),
+    )
       .filter(([_, value]) => !!value)
       .sort(([keyA], [keyB]) => orderKeys.indexOf(keyA) - orderKeys.indexOf(keyB))
       .map(([k, v]) => [k, order.find(({ key }) => key === k)?.sort(v)]),
--- packages/sort/test/sort.test.ts
+++ packages/sort/test/sort.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest'
 import { sort } from '../src'
 
 describe('sort', () => {
-  it('basics', () => {
+  it('object', () => {
     const json = sort({
       name: '@package-json/test',
       private: true,
@@ -12,4 +12,24 @@ describe('sort', () => {
 
     expect(Object.keys(json)).toStrictEqual(['name', 'type', 'private'])
   })
+
+  it('string', () => {
+    const json = sort(JSON.stringify({
+      name: '@package-json/test',
+      private: true,
+      type: 'module',
+    }))
+
+    expect(Object.keys(json)).toStrictEqual(['name', 'type', 'private'])
+  })
+
+  // it('template', () => {
+  //   const json = sort`{
+  //     "name": "@package-json/test",
+  //     "private": true,
+  //     "type": "module"
+  //   }`
+
+  //   expect(Object.keys(json)).toStrictEqual(['name', 'type', 'private'])
+  // })
 })
