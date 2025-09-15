b9db854cacd1b7fa7e9561284af125c24d313bc2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-15 22:05:08 +0800

    chore: renew

--- bump.config.ts
+++ bump.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'bumpp'
-import { globSync as glob } from 'node:fs'
 
 export default defineConfig({
-  files: glob('**/package.json'),
+  all: true,
+  push: false,
+  recursive: true,
 })
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,1 @@
-import type { Linter } from 'eslint'
-
-import antfu from '@antfu/eslint-config'
-import ii from '@importantimport/eslint-config'
-
-export default antfu({
-  jsonc: true,
-  test: true,
-  typescript: true,
-})
-  .append(ii({ functional: false })) as Linter.Config
+export { default } from '@moeru/eslint-config'
